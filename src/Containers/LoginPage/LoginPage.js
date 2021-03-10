e4c6c88243c7d270db82d98b3900786c5f9e1a62
Tomolo997 <tomazovsenjak7@gmail.com>
2021-03-10 10:11:15 +0100

    added the initial login page

--- src/Containers/LoginPage/LoginPage.js
+++ src/Containers/LoginPage/LoginPage.js
@@ -1,9 +1,13 @@
 import React from 'react';
 import Navbar from '../../Components/Navbar/Navbar';
+import Footer from '../../Components/Footer/Footer';
+import FormSection from '../LoginSection/LoginSection';
 const LoginPage = () => {
   return (
     <>
       <Navbar />
+      <FormSection />
+      <Footer />
     </>
   );
 };
