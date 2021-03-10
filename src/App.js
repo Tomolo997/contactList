4e7b9bb0dde739b52fcb4a1b6016236f82007d98
Tomolo997 <tomazovsenjak7@gmail.com>
2021-03-10 09:46:10 +0100

    added the footer

--- src/App.js
+++ src/App.js
@@ -2,12 +2,14 @@ import './App.css';
 import Navbar from './Container/Navbar/Navbar';
 import { BrowserRouter } from 'react-router-dom';
 import FirstPage from './Components/FirstPage/FirstPage';
+import Footer from './Components/Footer/Footer';
 function App() {
   return (
     <div className="App">
       <BrowserRouter>
         <Navbar />
         <FirstPage />
+        <Footer />
       </BrowserRouter>
     </div>
   );
