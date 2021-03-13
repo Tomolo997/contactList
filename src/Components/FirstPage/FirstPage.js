1c44add6e5344849dff005bd93dcfd356d40bdda
Tomolo997 <tomazovsenjak7@gmail.com>
2021-03-13 07:38:37 +0100

    you can add contacts

--- src/Components/FirstPage/FirstPage.js
+++ src/Components/FirstPage/FirstPage.js
@@ -4,16 +4,27 @@ import { CredentialContext } from '../../App';
 import Contacts from '../Contacts/Contacts';
 const FirstPage = () => {
   const [credentials] = useContext(CredentialContext);
-  return (
-    <div className={classes.FirstPage}>
+  let showWelcome = (
+    <div>
       <h1 className={classes.Heading1}>
         Contact<span className={classes.ListSpan}>List</span>{' '}
       </h1>
       <p className={classes.Paragraph}>Sign up to manage your Contact list</p>
-      <p className={classes.Paragraph}>
-        Welcome {credentials && credentials.username}
-        {/* if the credentials are valid and they exist then base out the Contacts */}
-      </p>
+      <p className={classes.Paragraph}></p>
+    </div>
+  );
+  return (
+    <div className={classes.FirstPage}>
+      {credentials ? (
+        <h1 className={classes.Welcome}>
+          {' '}
+          Welcome {credentials && credentials.username}
+        </h1>
+      ) : (
+        showWelcome
+      )}
+
+      {/* if the credentials are valid and they exist then base out the Contacts */}
       {credentials && <Contacts />}
     </div>
   );
